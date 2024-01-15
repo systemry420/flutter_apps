a4d92d370a691b8d51ace97449fa8a7c5add33d5
kassem <kassem@oobconsulting.com>
2024-01-15 22:01:38 +0200

    extract Counter class to new file

---
+++ lib/Counter.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class Counter extends StatefulWidget {
+  const Counter({super.key});
+
+  @override
+  State<Counter> createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text(
+            counter.toString(),
+            style: const TextStyle(fontSize: 200),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    counter++;
+                  });
+                },
+                icon: const Icon(Icons.arrow_circle_up),
+                color: Colors.green,
+                iconSize: 100,
+              ),
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    counter--;
+                  });
+                },
+                icon: const Icon(Icons.arrow_circle_down),
+                color: Colors.red,
+                iconSize: 100,
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_apps/Counter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,57 +23,6 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.blue,
         ),
         body: Counter(),
-      ),
-    );
-  }
-}
-
-class Counter extends StatefulWidget {
-  const Counter({super.key});
-
-  @override
-  State<Counter> createState() => _CounterState();
-}
-
-class _CounterState extends State<Counter> {
-  int counter = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Text(
-            counter.toString(),
-            style: const TextStyle(fontSize: 200),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              IconButton(
-                onPressed: () {
-                  setState(() {
-                    counter++;
-                  });
-                },
-                icon: const Icon(Icons.arrow_circle_up),
-                color: Colors.green,
-                iconSize: 100,
-              ),
-              IconButton(
-                onPressed: () {
-                  setState(() {
-                    counter--;
-                  });
-                },
-                icon: const Icon(Icons.arrow_circle_down),
-                color: Colors.red,
-                iconSize: 100,
-              )
-            ],
-          ),
-        ],
       ),
     );
   }
