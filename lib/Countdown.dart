18c34ebeeebd855450b244d72d6c3320f12d893b
kassem <kassem@oobconsulting.com>
2024-01-18 20:03:45 +0200

    implement the logic of calculating the difference between 2 dates

--- lib/Countdown.dart
+++ lib/Countdown.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class Countdown extends StatefulWidget {
@@ -10,22 +12,76 @@ class Countdown extends StatefulWidget {
 class _CountdownState extends State<Countdown> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
+    DateTime dateTimeAtFuture = DateTime(2024, 12, 31);
+    DateTime dateTimeNow = DateTime.now();
+
+    var differenceInSeconds = dateTimeAtFuture.difference(dateTimeNow).inSeconds;
+    var seconds = differenceInSeconds;
+    var days = seconds ~/ 86400;
+    seconds %= 86400;
+    var hours = seconds ~/ 3600;
+    seconds %= 3600;
+    var minutes = seconds ~/ 60;
+    seconds %= 60;
+
+    Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        differenceInSeconds = dateTimeAtFuture.difference(dateTimeNow).inSeconds;
+        days = seconds ~/ 86400;
+        seconds %= 86400;
+        hours = seconds ~/ 3600;
+        seconds %= 3600;
+        minutes = seconds ~/ 60;
+        seconds %= 60;
+      });
+    });
+    return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Padding(
-            padding: EdgeInsets.only(bottom: 300.0),
-            child: Text('Countdown', style: TextStyle(fontSize: 40),),
+          const Padding(
+            padding: EdgeInsets.only(bottom: 120.0),
+            child: Column(
+              children: [
+                Text('Countdown', style: TextStyle(fontSize: 40),),
+                Icon(Icons.alarm, size: 100, color: Colors.purple,),
+              ],
+            )
           ),
+
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Text('00', style: TextStyle(fontSize: 40),),
-              Text('00', style: TextStyle(fontSize: 40),),
-              Text('00', style: TextStyle(fontSize: 40),),
-              Text('00', style: TextStyle(fontSize: 40),),
-              Text('00', style: TextStyle(fontSize: 40),),
+              SafeArea(
+                child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Column(
+                          children: [
+                            Text('${ days < 10 ? "0$days" : days}', style: TextStyle(fontSize: 50),),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Text('${ hours < 10 ? "0$hours" : hours }', style: TextStyle(fontSize: 50),),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Text('${ minutes }', style: TextStyle(fontSize: 50),),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Text('$seconds', style: TextStyle(fontSize: 50),),
+                          ],
+                        ),
+                      ],
+                    ),
+                ),
+              ),
             ],
           ),
           Row(
