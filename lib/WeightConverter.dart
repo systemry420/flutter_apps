7c6d7cdd8531a810ef04906f375558a7b59e1ecc
kassem <kassem@oobconsulting.com>
2024-01-19 21:14:08 +0200

    main layout

--- lib/WeightConverter.dart
+++ lib/WeightConverter.dart
@@ -10,12 +10,80 @@ class WeightConverter extends StatefulWidget {
 class _WeightConverterState extends State<WeightConverter> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: const Column(
-        children: [
-          Text('Weight Converter')
-        ],
-      ),
+    return Column(
+      children: [
+        Text('Weight Converter', style: TextStyle(fontSize: 40),),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Container(
+                  alignment: Alignment.centerLeft,
+                  color: Colors.redAccent,
+                  child: Padding(
+                    padding: const EdgeInsets.all(32),
+                    child: Column(
+                      children: [
+                        Text('Gram'),
+                        Text('Gram'),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Container(
+                  color: Colors.lightGreen,
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.all(32.0),
+                    child: Column(
+                      children: [
+                        Text('Gram'),
+                        Text('Gram'),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Container(
+                  color: Colors.lightBlue,
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.all(32.0),
+                    child: Column(
+                      children: [
+                        Text('Gram'),
+                        Text('Gram'),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        )
+
+      ],
     );
   }
 }
