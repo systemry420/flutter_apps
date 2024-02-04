ce4c8310147d58ae65584df39c79a0ec78d21dc3
kassem <kassem@oobconsulting.com>
2024-02-04 18:05:50 +0200

    using reusable Card container, and adding Expanded to layout them

--- lib/BMI/InputPage.dart
+++ lib/BMI/InputPage.dart
@@ -10,27 +10,41 @@ class InputPage extends StatefulWidget {
 class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
       children: [
-        Container(
-          height: 200,
-          width: 180,
-          margin: EdgeInsets.all(16),
-          decoration: BoxDecoration(
-              color: const Color(0xFF1D1E33),
-              borderRadius: BorderRadius.circular(10)
+        Expanded(
+          child: Row(
+            children: [
+              Expanded(child: Card()),
+              Expanded(child: Card()),
+            ],
           ),
         ),
-        Container(
-          height: 200,
-          width: 180,
-          margin: EdgeInsets.all(16),
-          decoration: BoxDecoration(
-              color: const Color(0xFF1D1E33),
-              borderRadius: BorderRadius.circular(10)
+        Expanded(child: Card()),
+        Expanded(
+          child: Row(
+            children: [
+              Expanded(child: Card()),
+              Expanded(child: Card()),
+            ],
           ),
-        )
-      ]
+        ),
+      ],
+    );
+  }
+}
+
+class Card extends StatelessWidget {
+  const Card({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Card'),
+      margin: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+          color: const Color(0xFF1D1E33),
+          borderRadius: BorderRadius.circular(10)),
     );
   }
 }
